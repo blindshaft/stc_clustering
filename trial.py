021394b4c98531eb19dae42745b65449b2eda8e8
blindshaft <32421131+blindshaft@users.noreply.github.com>
2020-12-28 19:09:02 -0500

    add new file

---
+++ trial.py
@@ -0,0 +1,7 @@
+import os
+from collections import Counter
+import nltk
+import numpy as np
+import scipy.io
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.preprocessing import MinMaxScaler
